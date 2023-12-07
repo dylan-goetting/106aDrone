fa5b89bf01afe2fd436bff99f1ec7c9a59221e94
skrider <skrider@berkeley.edu>
2023-12-07 13:53:43 -0800

    add async video stream

--- capture_frame_udp.py
+++ capture_frame_udp.py
@@ -1,24 +1,48 @@
 import cv2
+import os
 import time
 import socket
+from async_video_stream import AsyncPositionStream
 
-local_addr = ('', 11111)
-# tello_addr = ('192.168.10.1', 11111)
+ITER = 30 * 5
 
-# Create a UDP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(local_addr)
+HOST = '0.0.0.0'
+PORT = 11111
 
-iter = 30 * 5
+def capture_udp(iter) -> float:
+    local_addr = ('', PORT)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(local_addr)
 
-print("starting capture frames")
-perf_start = time.perf_counter()
-for _ in range(iter):
-    while True:
-        buf, _ = sock.recvfrom(2048)
-        if len(buf) != 1460:
-            break
-    print("frame recieved")
-perf_end = time.perf_counter()
+    print("starting capture frames")
+    for _ in range(iter):
+        h264_frame = None
+        frame = b''
+        buf = None
+        while buf == None or len(buf) == 1460:
+            buf, _ = sock.recvfrom(2048)
+            frame += buf
+        print("frame")
 
-print(f"FPS: {iter / (perf_end - perf_start)}")
+def capture_cv2(iter) -> float:
+    cap = cv2.VideoCapture(f"udp://{HOST}:{PORT}")
+    print("starting capture frames")
+    ret, frame = cap.read()
+    for _ in range(iter):
+        ret, frame = cap.read()
+
+def capture_async_pos():
+    vs = AsyncPositionStream(False)
+    print("starting capture frames")
+    for i in range(10000):
+        os.system("clear")
+        print(vs.pos())
+
+    print(vs.get_fps())
+
+
+# perf_start = time.perf_counter()
+# capture_cv2(ITER)
+# perf_end = time.perf_counter()
+# print(f"FPS: {ITER / (perf_end - perf_start)}")
+capture_async_pos()
